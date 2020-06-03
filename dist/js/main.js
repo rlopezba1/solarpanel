fbbfe24e5f9e948d25059c28749c9edd2be3257b
Ruben <rubenlopezb@gmail.com>
2020-06-03 16:23:51 +0100

    new changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,31 +1,31 @@
-const menuBtn = document.querySelector('.menu-btn');
-const hamburger = document.querySelector('.menu-btn_burger');
-const nav = document.querySelector('.nav');
-const menuNav = document.querySelector('.menu-nav');
-const navItems = document.querySelectorAll('.menu-nav_item');
+// const menuBtn = document.querySelector('.menu-btn');
+// const hamburger = document.querySelector('.menu-btn_burger');
+// const nav = document.querySelector('.nav');
+// const menuNav = document.querySelector('.menu-nav');
+// const navItems = document.querySelectorAll('.menu-nav_item');
 
 
-let showMenu = false;
+// let showMenu = false;
 
-menuBtn.addEventListener('click',toggleMenu)
+// menuBtn.addEventListener('click',toggleMenu)
 
-function toggleMenu(){
-    if(!showMenu) {
-        hamburger.classList.add('open');
-        nav.classList.add('open');
-        menuNav.classList.add('open');
-        navItems.forEach(item => item.classList.add('open'));
+// function toggleMenu(){
+//     if(!showMenu) {
+//         hamburger.classList.add('open');
+//         nav.classList.add('open');
+//         menuNav.classList.add('open');
+//         navItems.forEach(item => item.classList.add('open'));
 
-        showMenu = true;
-    } else {
-        hamburger.classList.remove('open');
-        nav.classList.remove('open');
-        menuNav.classList.remove('open');
-        navItems.forEach(item => item.classList.remove('open'));
+//         showMenu = true;
+//     } else {
+//         hamburger.classList.remove('open');
+//         nav.classList.remove('open');
+//         menuNav.classList.remove('open');
+//         navItems.forEach(item => item.classList.remove('open'));
 
-        showMenu = false;
-    }
-}
+//         showMenu = false;
+//     }
+// }
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
